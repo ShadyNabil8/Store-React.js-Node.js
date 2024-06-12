d86425d59a52df3929f7457311d67072a39bf80c
shady <shadyngheith@gmail.com>
2024-06-13 02:32:32 +0300

    Add display controller

--- frontend/src/components/Item/Item.jsx
+++ frontend/src/components/Item/Item.jsx
@@ -3,30 +3,51 @@ import { assets } from '../../assets/assets'
 import './Item.css'
 import { FaCartArrowDown } from "react-icons/fa";
 import { IoArrowRedoCircle } from "react-icons/io5";
+import { MdArrowRightAlt } from "react-icons/md";
 import { itemsContext } from '../../Contexts/StoreContext'
-
-const Item = ({ id, name, image, price, category }) => {
+const Item = ({ id, name, image, price, category, display }) => {
     const { cartItems, addToCart, removeFromCart } = useContext(itemsContext);
-
     return (
-        <div className='item'>
-            <div className="item-image">
-                <img src={image}></img>
-            </div>
-
-            <div className="item-name">
-                <p id='name'>{name}</p>
-            </div>
+        <div id='item' className={display === 'flex' ? 'flex-item' : 'grid-item'}>
+            {
+                (display === 'flex')
+                    ? (
+                        <>
+                            <div className="item-image">
+                                <img src={image}></img>
+                            </div>
+                            <div className="item-name">
+                                <p id='name'>{name}</p>
+                            </div>
+                        </>
+                    )
+                    : (
+                        <>
+                            <div className="item-name">
+                                <p id='name'>{name}</p>
+                            </div>
+                            <div className="item-image">
+                                <img src={image}></img>
+                            </div>
+                        </>
+                    )
+            }
             <div className="item-cart">
-                <p id='price'>EGP {price}</p>
+                <p id='price' className='price'>EGP {price}</p>
                 {
-                    !cartItems[id]
-                        ? <div className='cart-icon-div' onClick={() => { addToCart(id) }}><FaCartArrowDown className='cart-icon' /></div>
-                        : <div className='view-cart-icon-div'><IoArrowRedoCircle className='cart-icon' /></div>
+                    display === 'flex'
+                        ? (
+                            <button className='add-btn' onClick={!cartItems[id] ? () => { addToCart(id) } : () => {/*do nothing until view cart*/ }}>
+                                {!cartItems[id] ? 'Add To Cart' : <>View Cart <MdArrowRightAlt /></>}
+                            </button>
+                        )
+                        : (
+                            !cartItems[id]
+                                ? <div className='cart-icon-div' onClick={() => { addToCart(id) }}><FaCartArrowDown className='cart-icon' /></div>
+                                : <div className='view-cart-icon-div'><IoArrowRedoCircle className='cart-icon' /></div>
+                        )
                 }
             </div>
-
-
         </div>
     )
 }
--- frontend/src/components/ItemsList/ItemsList.jsx
+++ frontend/src/components/ItemsList/ItemsList.jsx
@@ -1,25 +1,61 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useContext } from 'react'
 import { itemsContext } from '../../Contexts/StoreContext'
 import Item from '../Item/Item'
 import './ItemsList.css'
+import { FaListUl } from "react-icons/fa6";
+import { BsFillGrid3X3GapFill } from "react-icons/bs";
+
 
 const ItemsList = ({ selectedCategory }) => {
+    const [display, setDisplay] = useState('flex');
     const { items_list } = useContext(itemsContext)
+
     return (
-        <>
-            <div className='items-list'>
+        <div className='items-view'>
+            <div className='display-control'>
+                <div className='display-icons'>
+                    <BsFillGrid3X3GapFill 
+                            className='grid-icon' 
+                            onClick={() => { setDisplay('grid') }} 
+                            style={(display==='grid')?{opacity:1}:{opacity:0.5}}
+                    />
+                    <FaListUl 
+                            className='list-icon' 
+                            onClick={() => { setDisplay('flex') }} 
+                            style={(display==='flex')?{opacity:1}:{opacity:0.5}}
+                    />
+                </div>
+                <div className='filters'>
+                    <select name="sorting" id="">
+                        <option value="default">Default sorting</option>
+                        <option value="popularity">Sort by popularity</option>
+                        <option value="average">Sort by average rating</option>
+                        <option value="latest">Sort by latest</option>
+                        <option value="price_h_l">Sort by price: high to low</option>
+                        <option value="price_l_h">Sort by price: low to hight</option>
+                    </select>
+                    <select name="batching" id="">
+                        <option value="all">Show all</option>
+                        <option value="32">Show 32</option>
+                        <option value="16">Show 16</option>
+                    </select>
+                </div>
+            </div>
+            <div className={display === 'flex' ? 'items-list-flex' : 'items-list-grid'}>
                 {
                     items_list.map((item, index) => {
                         if (selectedCategory === 'all' || selectedCategory === item.category) {
                             return (
-                                <Item key={index} id={item._id} name={item.name} price={item.price} image={item.image}></Item>
+                                <Item key={index} id={item._id} name={item.name} price={item.price} image={item.image} display={display}>
+
+                                </Item>
                             )
                         }
                     })
                 }
             </div>
-        </>
+        </div>
 
     )
 }
